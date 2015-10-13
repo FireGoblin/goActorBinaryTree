e94f0111bb8917adc20b7218c8484e5c437ee1af
Michael Overstreet <firegoblin21@gmail.com>
2015-10-13 16:10:54 -0400

    slim channels improves response time but increases sending messages

--- test_probe_test.go
+++ test_probe_test.go
@@ -6,123 +6,123 @@ import (
 	"time"
 )
 
-// func TestinsertsAndcontains(t *testing.T) {
-// 	testProbe := maketestProbe()
+func TestinsertsAndcontains(t *testing.T) {
+	testProbe := maketestProbe()
 
-// 	one := testProbe.makecontains(1)
-// 	testProbe.sendoperation(one)
-// 	oneResult := <-testProbe.childReply
+	one := testProbe.makecontains(1)
+	testProbe.sendoperation(one)
+	oneResult := <-testProbe.childReply
 
-// 	x, ok := oneResult.(containsResult)
-// 	if ok {
-// 		if x.ID() != 1 || x.Result() || !testProbe.checkReply(x) {
-// 			t.FailNow()
-// 		}
-// 	} else {
-// 		t.FailNow()
-// 	}
+	x, ok := oneResult.(containsResult)
+	if ok {
+		if x.ID() != 1 || x.Result() || !testProbe.checkReply(x) {
+			t.FailNow()
+		}
+	} else {
+		t.FailNow()
+	}
 
-// 	two := testProbe.makeinsert(1)
-// 	three := testProbe.makecontains(1)
-// 	testProbe.sendoperation(two)
-// 	testProbe.sendoperation(three)
+	two := testProbe.makeinsert(1)
+	three := testProbe.makecontains(1)
+	testProbe.sendoperation(two)
+	testProbe.sendoperation(three)
 
-// 	twoResult := <-testProbe.childReply
-// 	threeResult := <-testProbe.childReply
+	twoResult := <-testProbe.childReply
+	threeResult := <-testProbe.childReply
 
-// 	y, ok := twoResult.(operationFinished)
-// 	if ok {
-// 		if y.ID() != 2 || !testProbe.checkReply(y) {
-// 			t.FailNow()
-// 		}
-// 	} else {
-// 		t.FailNow()
-// 	}
+	y, ok := twoResult.(operationFinished)
+	if ok {
+		if y.ID() != 2 || !testProbe.checkReply(y) {
+			t.FailNow()
+		}
+	} else {
+		t.FailNow()
+	}
 
-// 	x, ok = threeResult.(containsResult)
-// 	if ok {
-// 		if x.ID() != 3 || !x.Result() || !testProbe.checkReply(x) {
-// 			t.FailNow()
-// 		}
-// 	} else {
-// 		t.FailNow()
-// 	}
+	x, ok = threeResult.(containsResult)
+	if ok {
+		if x.ID() != 3 || !x.Result() || !testProbe.checkReply(x) {
+			t.FailNow()
+		}
+	} else {
+		t.FailNow()
+	}
 
-// 	fmt.Println("inserts and contains succeeded")
-// }
+	fmt.Println("inserts and contains succeeded")
+}
 
-// func TestInstructionExample(t *testing.T) {
-// 	fmt.Println("--------------------------")
-// 	testProbe := maketestProbe()
+func TestInstructionExample(t *testing.T) {
+	fmt.Println("--------------------------")
+	testProbe := maketestProbe()
 
-// 	succeed := make(chan int)
-// 	fail := make(chan int)
+	succeed := make(chan int)
+	fail := make(chan int)
 
-// 	go testProbe.run(succeed, fail)
+	go testProbe.run(succeed, fail)
 
-// 	var ops []operation
-// 	ops = append(ops, testProbe.makeinsert(1))
-// 	ops = append(ops, testProbe.makecontains(2))
-// 	ops = append(ops, testProbe.makeremove(1))
-// 	ops = append(ops, testProbe.makeinsert(2))
-// 	ops = append(ops, testProbe.makecontains(1))
-// 	ops = append(ops, testProbe.makecontains(2))
+	var ops []operation
+	ops = append(ops, testProbe.makeinsert(1))
+	ops = append(ops, testProbe.makecontains(2))
+	ops = append(ops, testProbe.makeremove(1))
+	ops = append(ops, testProbe.makeinsert(2))
+	ops = append(ops, testProbe.makecontains(1))
+	ops = append(ops, testProbe.makecontains(2))
 
-// 	for _, op := range ops {
-// 		testProbe.sendoperation(op)
-// 		time.Sleep(1 * time.Millisecond)
-// 	}
+	for _, op := range ops {
+		testProbe.sendoperation(op)
+		time.Sleep(1 * time.Millisecond)
+	}
 
-// 	select {
-// 	case <-succeed:
-// 	case <-fail:
-// 		t.Fatal("Test failed. received message on fail channel.")
-// 	}
-// }
+	select {
+	case <-succeed:
+	case <-fail:
+		t.Fatal("Test failed. received message on fail channel.")
+	}
+}
 
-// func TestSmallgc(t *testing.T) {
-// 	fmt.Println("--------------------------")
-// 	testProbe := maketestProbe()
+func TestSmallgc(t *testing.T) {
+	fmt.Println("--------------------------")
+	testProbe := maketestProbe()
 
-// 	succeed := make(chan int)
-// 	fail := make(chan int)
+	succeed := make(chan int)
+	fail := make(chan int)
 
-// 	go testProbe.run(succeed, fail)
+	go testProbe.run(succeed, fail)
 
-// 	var opsBefore []operation
-// 	var opsAfter []operation
+	var opsBefore []operation
+	var opsAfter []operation
 
-// 	opsBefore = append(opsBefore, testProbe.makeinsert(-122))
-// 	opsBefore = append(opsBefore, testProbe.makeinsert(99))
-// 	opsBefore = append(opsBefore, testProbe.makeinsert(-13))
-// 	opsBefore = append(opsBefore, testProbe.makeinsert(104))
-// 	opsBefore = append(opsBefore, testProbe.makeremove(-122))
+	opsBefore = append(opsBefore, testProbe.makeinsert(-122))
+	opsBefore = append(opsBefore, testProbe.makeinsert(99))
+	opsBefore = append(opsBefore, testProbe.makeinsert(-13))
+	opsBefore = append(opsBefore, testProbe.makeinsert(104))
+	opsBefore = append(opsBefore, testProbe.makeremove(-122))
 
-// 	opsAfter = append(opsAfter, testProbe.makecontains(-122))
-// 	opsAfter = append(opsAfter, testProbe.makecontains(99))
-// 	opsAfter = append(opsAfter, testProbe.makecontains(-13))
-// 	opsAfter = append(opsAfter, testProbe.makecontains(104))
-// 	opsAfter = append(opsAfter, testProbe.makecontains(777))
+	opsAfter = append(opsAfter, testProbe.makecontains(-122))
+	opsAfter = append(opsAfter, testProbe.makecontains(99))
+	opsAfter = append(opsAfter, testProbe.makecontains(-13))
+	opsAfter = append(opsAfter, testProbe.makecontains(104))
+	opsAfter = append(opsAfter, testProbe.makecontains(777))
 
-// 	for _, op := range opsBefore {
-// 		testProbe.sendoperation(op)
-// 		time.Sleep(1 * time.Millisecond)
-// 	}
+	for _, op := range opsBefore {
+		testProbe.sendoperation(op)
+		time.Sleep(1 * time.Millisecond)
+	}
 
-// 	testProbe.injectgc()
-// 	time.Sleep(1 * time.Millisecond)
+	testProbe.injectgc()
+	time.Sleep(1 * time.Millisecond)
 
-// 	for _, op := range opsAfter {
-// 		testProbe.sendoperation(op)
-// 		time.Sleep(1 * time.Millisecond)
-// 	}
+	for _, op := range opsAfter {
+		testProbe.sendoperation(op)
+		time.Sleep(1 * time.Millisecond)
+	}
 
-// 	select {
-// 	case <-succeed:
-// 	case <-fail:
-// 		t.Fatal("Test failed. received message on fail channel.")
-// 	}
-// }
+	select {
+	case <-succeed:
+	case <-fail:
+		t.Fatal("Test failed. received message on fail channel.")
+	}
+}
 
 func TestWorkWithgc(t *testing.T) {
 	fmt.Println("--------------------------")
